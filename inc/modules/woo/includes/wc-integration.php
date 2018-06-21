86f62178f35e41ffee6bd9ecf8ee337707376127
SashkoWooTeam <sashaosaylenko@gmail.com>
2018-06-21 18:49:45 +0300

    UPD : add to cart filter name

--- inc/modules/woo/includes/wc-integration.php
+++ inc/modules/woo/includes/wc-integration.php
@@ -145,7 +145,7 @@ if ( ! function_exists( 'kava_wc_cart_link_fragment' ) ) {
 		return $fragments;
 	}
 }
-add_filter( 'woocommerce_add_to_cart_fragments', 'kava_wc_cart_link_fragment' );
+add_filter( 'add_to_cart_fragments', 'kava_wc_cart_link_fragment' );
 
 if ( ! function_exists( 'kava_wc_cart_link' ) ) {
 	/**
