44cadd059dde2fba449db5cf5f7d5e1b169337ed
Simon Milz <simon.milz@tourware.net>
2020-12-16 11:37:09 +0100

    added zip script

---
+++ zip.php
@@ -0,0 +1,34 @@
+<?php
+
+$rootPath = realpath('.');
+
+$zip = new ZipArchive();
+$zip->open('tourware.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+$zip->addEmptyDir('tourware');
+
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+foreach ($files as $name => $file)
+{
+    if (!$file->isDir())
+    {
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        if (
+            substr($relativePath, 0, 6) !== '.github' &&
+            substr($relativePath, 0, 4) !== '.git' &&
+            substr($relativePath, 0, 5) !== '.idea' &&
+            substr($relativePath, 0, 7) !== 'zip.php'
+        ) {
+            $zip->addFile($filePath, 'tourware\\' . $relativePath);
+
+        }
+    }
+}
+
+$zip->close();
