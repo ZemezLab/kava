29249297f7e20926b7fdec08564dfa8457da5fd7
mono <luzinama@gmail.com>
2021-01-19 00:27:46 +0700

    THEME-79: countries widget

---
+++ tourware/Elementor/Widget/Travel/Countries.php
@@ -0,0 +1,292 @@
+<?php
+namespace Tourware\Elementor\Widget\Travel;
+
+use Elementor\Controls_Manager;
+use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
+use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
+use Elementor\Group_Control_Typography;
+use Elementor\Icons_Manager;
+use Elementor\Repeater;
+use Tourware\Elementor\Widget;
+use Tourware\Path;
+
+class Countries extends Widget {
+    /**
+     * @return string
+     */
+    public function get_name()
+    {
+        return 'tourware-countries';
+    }
+
+    /**
+     * @return string
+     */
+    public function get_title()
+    {
+        return __( 'Countries' );
+    }
+
+    protected function _register_controls() {
+        $this->start_controls_section('options', array(
+            'label' => esc_html__('Options'),
+        ));
+
+        $posts = wp_list_pluck(get_posts(['post_type' => ['tytotravels'], 'post_status' => 'publish', 'posts_per_page' => -1]), 'post_title', 'ID');
+        $this->add_control(
+            'post',
+            [
+                'label' => __('Post', 'tyto'),
+                'type' => Controls_Manager::SELECT2,
+                'options' => $posts,
+                'default' => in_array(get_post_type(get_the_ID()), ['tytotravels']) ? get_the_ID() : ''
+            ]
+        );
+
+        $this->end_controls_section();
+
+
+        $this->start_controls_section(
+            'section_icon_list',
+            [
+                'label' => __( 'List', 'elementor' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_responsive_control(
+            'space_between',
+            [
+                'label' => __( 'Space Between', 'elementor' ),
+                'type' => Controls_Manager::SLIDER,
+                'range' => [
+                    'px' => [
+                        'max' => 50,
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-items:not(.elementor-inline-items) .elementor-icon-list-item:not(:last-child)' => 'padding-bottom: calc({{SIZE}}{{UNIT}}/2)',
+                    '{{WRAPPER}} .elementor-icon-list-items:not(.elementor-inline-items) .elementor-icon-list-item:not(:first-child)' => 'margin-top: calc({{SIZE}}{{UNIT}}/2)',
+                    '{{WRAPPER}} .elementor-icon-list-items.elementor-inline-items .elementor-icon-list-item' => 'margin-right: calc({{SIZE}}{{UNIT}}/2); margin-left: calc({{SIZE}}{{UNIT}}/2)',
+                    '{{WRAPPER}} .elementor-icon-list-items.elementor-inline-items' => 'margin-right: calc(-{{SIZE}}{{UNIT}}/2); margin-left: calc(-{{SIZE}}{{UNIT}}/2)',
+                    'body.rtl {{WRAPPER}} .elementor-icon-list-items.elementor-inline-items .elementor-icon-list-item:after' => 'left: calc(-{{SIZE}}{{UNIT}}/2)',
+                    'body:not(.rtl) {{WRAPPER}} .elementor-icon-list-items.elementor-inline-items .elementor-icon-list-item:after' => 'right: calc(-{{SIZE}}{{UNIT}}/2)',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'icon_align',
+            [
+                'label' => __( 'Alignment', 'elementor' ),
+                'type' => Controls_Manager::CHOOSE,
+                'options' => [
+                    'left' => [
+                        'title' => __( 'Left', 'elementor' ),
+                        'icon' => 'eicon-h-align-left',
+                    ],
+                    'center' => [
+                        'title' => __( 'Center', 'elementor' ),
+                        'icon' => 'eicon-h-align-center',
+                    ],
+                    'right' => [
+                        'title' => __( 'Right', 'elementor' ),
+                        'icon' => 'eicon-h-align-right',
+                    ],
+                ],
+                'prefix_class' => 'elementor%s-align-',
+            ]
+        );
+
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'section_icon_style',
+            [
+                'label' => __( 'Icon', 'elementor' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+            'icon_color',
+            [
+                'label' => __( 'Color', 'elementor' ),
+                'type' => Controls_Manager::COLOR,
+                'default' => '',
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-icon i' => 'color: {{VALUE}};',
+                    '{{WRAPPER}} .elementor-icon-list-icon svg' => 'fill: {{VALUE}};',
+                ],
+                'global' => [
+                    'default' => Global_Colors::COLOR_PRIMARY,
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'icon_color_hover',
+            [
+                'label' => __( 'Hover', 'elementor' ),
+                'type' => Controls_Manager::COLOR,
+                'default' => '',
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-item:hover .elementor-icon-list-icon i' => 'color: {{VALUE}};',
+                    '{{WRAPPER}} .elementor-icon-list-item:hover .elementor-icon-list-icon svg' => 'fill: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'icon_size',
+            [
+                'label' => __( 'Size', 'elementor' ),
+                'type' => Controls_Manager::SLIDER,
+                'default' => [
+                    'size' => 14,
+                ],
+                'range' => [
+                    'px' => [
+                        'min' => 6,
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-icon i' => 'font-size: {{SIZE}}{{UNIT}};',
+                    '{{WRAPPER}} .elementor-icon-list-icon svg' => 'width: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'icon_self_align',
+            [
+                'label' => __( 'Alignment', 'elementor' ),
+                'type' => Controls_Manager::CHOOSE,
+                'options' => [
+                    'left' => [
+                        'title' => __( 'Left', 'elementor' ),
+                        'icon' => 'eicon-h-align-left',
+                    ],
+                    'center' => [
+                        'title' => __( 'Center', 'elementor' ),
+                        'icon' => 'eicon-h-align-center',
+                    ],
+                    'right' => [
+                        'title' => __( 'Right', 'elementor' ),
+                        'icon' => 'eicon-h-align-right',
+                    ],
+                ],
+                'default' => '',
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-icon' => 'text-align: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'section_text_style',
+            [
+                'label' => __( 'Text', 'elementor' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+            'text_color',
+            [
+                'label' => __( 'Text Color', 'elementor' ),
+                'type' => Controls_Manager::COLOR,
+                'default' => '',
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-text' => 'color: {{VALUE}};',
+                ],
+                'global' => [
+                    'default' => Global_Colors::COLOR_SECONDARY,
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'text_color_hover',
+            [
+                'label' => __( 'Hover', 'elementor' ),
+                'type' => Controls_Manager::COLOR,
+                'default' => '',
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-item:hover .elementor-icon-list-text' => 'color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'text_indent',
+            [
+                'label' => __( 'Text Indent', 'elementor' ),
+                'type' => Controls_Manager::SLIDER,
+                'range' => [
+                    'px' => [
+                        'max' => 50,
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .elementor-icon-list-text' => is_rtl() ? 'padding-right: {{SIZE}}{{UNIT}};' : 'padding-left: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            Group_Control_Typography::get_type(),
+            [
+                'name' => 'icon_typography',
+                'selector' => '{{WRAPPER}} .elementor-icon-list-item, {{WRAPPER}} .elementor-icon-list-item a',
+                'global' => [
+                    'default' => Global_Typography::TYPOGRAPHY_TEXT,
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    protected function render() {
+        $settings = $this->get_settings_for_display();
+
+        $post = $settings['post'] ? $settings['post'] : get_the_ID();
+        $t_countries = get_post_meta($post, 'tytocountries', true);
+        $countries = [];
+        if (!empty($t_countries)) {
+            foreach ($t_countries as $t_country) {
+                $countries[] = $t_country['official_name_de'];
+            }
+        }
+        if (empty($countries)) {
+            $countries[] = 'Uganda';
+        }
+        //TODO use countries taxonomy
+
+        $this->add_render_attribute( 'icon_list', 'class', 'elementor-icon-list-items' );
+        $this->add_render_attribute( 'icon_list', 'class', 'elementor-inline-items' );
+
+        $this->add_render_attribute( 'list_item', 'class', 'elementor-icon-list-item' );
+        $this->add_render_attribute( 'list_item', 'class', 'elementor-inline-item' );
+        ?>
+        <ul <?php echo $this->get_render_attribute_string( 'icon_list' ); ?>>
+            <?php
+            foreach ( $countries as $index => $item ) : ?>
+                <li <?php echo $this->get_render_attribute_string( 'list_item' ); ?>>
+                        <span class="elementor-icon-list-icon">
+							<?php Icons_Manager::render_icon( $settings['selected_icon'], [ 'aria-hidden' => 'true' ] ); ?>
+						</span>
+                    <span><?php echo $item; ?></span>
+
+                </li>
+            <?php
+            endforeach;
+            ?>
+        </ul>
+        <?php
+    }
+}
