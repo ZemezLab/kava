ac5b0dd089baff2dad4f1d6d44bd3da8737da760
mono <luzinama@gmail.com>
2021-03-14 22:39:24 +0700

    THEME-100: dates widget > inquiry & book buttons

--- tourware/Elementor/Widget/Travel/Dates.php
+++ tourware/Elementor/Widget/Travel/Dates.php
@@ -64,6 +64,45 @@ class Dates extends AbstractAccordion {
         );
 
         $this->add_control(
+            'show_inquiry_button',
+            [
+                'label' => __('Show Inquiry Button', 'elementor-pro'),
+                'type' => Controls_Manager::SWITCHER,
+                'label_on' => __('Show', 'elementor-pro'),
+                'label_off' => __('Hide', 'elementor-pro'),
+                'separator' => 'before',
+            ]
+        );
+
+        $this->add_control('inquiry_button_text',
+            [
+                'type'      => Controls_Manager::TEXT,
+                'label'     => esc_html__( 'Inquiry Button text', 'tourware' ),
+                'default'   => __('Inquiry', 'tourware'),
+                'condition' => [ 'show_inquiry_button' => 'yes' ]
+            ]
+        );
+
+        $this->add_control(
+            'show_book_button',
+            [
+                'label' => __('Show Book Button', 'elementor-pro'),
+                'type' => Controls_Manager::SWITCHER,
+                'label_on' => __('Show', 'elementor-pro'),
+                'label_off' => __('Hide', 'elementor-pro'),
+            ]
+        );
+
+        $this->add_control('book_button_text',
+            [
+                'type'      => Controls_Manager::TEXT,
+                'label'     => esc_html__( 'Book Button text', 'tourware' ),
+                'default'   => __('Book', 'tourware'),
+                'condition' => [ 'show_book_button' => 'yes' ]
+            ]
+        );
+
+        $this->add_control(
             'heading_year_filter',
             [
                 'label' => __( 'Years Filter', 'tourware' ),
@@ -294,7 +333,7 @@ class Dates extends AbstractAccordion {
         $this->end_controls_section();
 
         $this->start_controls_section(
-            'years_filter',
+            'years_filter_style',
             [
                 'label' => __( 'Years Filter', 'tourware' ),
                 'tab' => Controls_Manager::TAB_STYLE,
@@ -368,6 +407,105 @@ class Dates extends AbstractAccordion {
         ) );
 
         $this->end_controls_section();
+
+        $this->addControlGroup([
+            'id'=>'inquiry_button_style',
+            'type' => 'button',
+            'label' => 'Inquiry Button',
+            'selector' => '.inquiry-button',
+            'condition' => ['show_inquiry_button' => 'yes']
+        ]);
+
+        $this->addControlGroup([
+            'id'=>'book_button_style',
+            'type' => 'button',
+            'label' => 'Book Button',
+            'selector' => '.book-button',
+            'condition' => ['show_book_button' => 'yes']
+        ]);
+
+        $this->start_controls_section(
+            'date_buttons_style',
+            [
+                'label' => __( 'Date Buttons Alignment', 'elementor' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+                'conditions' => [
+                    'relation' => 'or',
+                    'terms' => [
+                        [
+                            'name' => 'show_inquiry_button',
+                            'operator' => '==',
+                            'value' => 'yes'
+                        ],
+                        [
+                            'name' => 'show_book_button',
+                            'operator' => '==',
+                            'value' => 'yes'
+                        ]
+                    ]
+                ]
+            ]
+        );
+
+        $this->add_control(
+            'date_buttons_position',
+            [
+                'label' => __( 'Buttons Position', 'tourware' ),
+                'type' => Controls_Manager::SELECT,
+                'options' => [
+                    'bottom' => __( 'Bottom', 'tourware' ),
+                    'right' => __( 'Right', 'tourware' ),
+                ],
+                'default' => 'right',
+            ]
+        );
+
+        $this->add_responsive_control(
+            'date_buttons_horizontal_align', array(
+            'type'           => Controls_Manager::CHOOSE,
+            'label'          => esc_html__( 'Alignment', 'elementor-pro' ),
+            'options'        => array(
+                'left'   => array(
+                    'title' => esc_html__( 'Left', 'elementor-pro' ),
+                    'icon'  => 'fa fa-align-left'
+                ),
+                'center' => array(
+                    'title' => esc_html__( 'Center', 'elementor-pro' ),
+                    'icon'  => 'fa fa-align-center'
+                ),
+                'right'  => array(
+                    'title' => esc_html__( 'Right', 'elementor-pro' ),
+                    'icon'  => 'fa fa-align-right'
+                ),
+            ),
+            'selectors'      => array(
+                '{{WRAPPER}} .date-buttons' => 'text-align: {{VALUE}};'
+            ),
+            'condition' => ['date_buttons_position' => 'bottom'],
+        ) );
+
+        $this->add_responsive_control(
+            'date_buttons_verticel_align', array(
+            'type'           => Controls_Manager::CHOOSE,
+            'label'          => esc_html__( 'Vertical Alignment', 'elementor-pro' ),
+            'options'        => array(
+                'flex-start'   => array(
+                    'title' => esc_html__( 'Top', 'elementor-pro' ),
+                    'icon'  => 'fa fa-arrow-up'
+                ),
+                'flex-end' => array(
+                    'title' => esc_html__( 'Bottom', 'elementor-pro' ),
+                    'icon'  => 'fa fa-arrow-down'
+                ),
+            ),
+            'default' => 'flex-start',
+            'selectors'      => array(
+                '{{WRAPPER}} .date-buttons' => 'align-items: {{VALUE}};'
+            ),
+            'condition' => ['date_buttons_position' => 'right'],
+        ) );
+
+        $this->end_controls_section();
     }
 
     protected function getTemplateData()
@@ -398,11 +536,11 @@ class Dates extends AbstractAccordion {
             $tab_title .= '<div class="days">'.date_diff($start, $end)->format('%d').' Tage</div>';
 
 
+            $free_places = $date->maxPax - $date->bookedPax;
             $tab_title .= '<div class="info">';
             if ($settings['show_places'] === 'yes') {
                 $tab_title .= '<div class="info-wrapper">';
                 $class = 'high-qty';
-                $free_places = $date->maxPax - $date->bookedPax;
                 $tooltip = $settings['places_high_tooltip'];
                 if ($free_places === 0) {
                     $class = 'low-qty';
@@ -441,7 +579,8 @@ class Dates extends AbstractAccordion {
             $tab_title .= '<div class="price" data-value="'.$price_value.'"><span class="value">'.$price_value.'</span>&nbsp;€</div>';
             $tab_title .= '</div></div>';
 
-            $tab_content = '';
+            $tab_content = '<div class="date-content buttons-'.($settings['date_buttons_position']).'">';
+            $tab_content .= '<div class="date-description">';
             if ($date->note) $tab_content .= '<div class="note">'.$date->note.'</div>';
             $tab_content .= '<h6>Zuschläge / Ermäßigungen</h6>';
             $tab_content .= '<div>* basierend auf dem Basispreis (1 Erwachsener im Doppelzimmer)</div>';
@@ -449,6 +588,20 @@ class Dates extends AbstractAccordion {
                 $tab_content .= '<div class="surcharge"><span class="h6-style">Einzelzimmer:</span> '.number_format($date->singleRoomSurcharge, 0, ',', '.').'&nbsp;€</div>';
             }
             if ($date->description) $tab_content .= '<div class="description">'.$date->description.'</div>';
+            $tab_content .= '</div>';
+
+            if ($settings['show_inquiry_button'] === 'yes' || $settings['show_book_button'] === 'yes') {
+                $tab_content .= '<div class="date-buttons">';
+                if ($settings['show_inquiry_button'] === 'yes') {
+                    $tab_content .= '<a href="#" class="elementor-button inquiry-button">'.$settings['inquiry_button_text'].'</a>';
+                }
+                if ($settings['show_book_button'] === 'yes' && $free_places > 0) {
+                    $tab_content .= '<a href="#" class="elementor-button book-button">'.$settings['book_button_text'].'</a>';
+                }
+                $tab_content .= '</div>';
+            }
+            $tab_content .= '</div>';
+
 
             $tab_data = [];
             $tab_data[] = ['name' => 'year', 'value' => $start->format('Y') ];
